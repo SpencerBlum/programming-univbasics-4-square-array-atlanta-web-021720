c0fd937228a643638e00b294f5323fa0259ccef8
Spencer Blum <spencerblum@gmail.com>
2020-01-18 18:17:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    new_num = array[counter] * array
+    new_num = array[counter] * array[counter]
     new_array.push = new_num
   end
 
